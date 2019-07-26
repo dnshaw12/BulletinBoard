d08da858599b1afbc0e1897291eaaa728a1861a6
Dan <dnshaw12@gmail.com>
2019-07-26 15:56:02 -0500

    set up db and server. both running

---
+++ server.js
@@ -0,0 +1,16 @@
+const express 			= require('express');
+const bodyParser 		= require('body-parser');
+const methodOverride	= require('method-override');
+const session      		= require('express-session');
+const app         		= express();
+const PORT 				= 3000;
+
+require('./db/db');
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log('listening..... on port '+PORT);
+});
