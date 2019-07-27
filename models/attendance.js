d8e4a6f6165ce70cc38f798ad499c33ac1a75f38
Dan <dnshaw12@gmail.com>
2019-07-27 14:59:58 -0500

    created attendance model

---
+++ models/attendance.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const attendanceSchema = new mongoose.Schema({
+	member: [{
+		type: mongoose.Schema.Types.ObjectId,
+    	ref: 'Member'
+	}],
+	event: [{
+		type: mongoose.Schema.Types.ObjectId,
+    	ref: 'Event'
+	}]
+})
