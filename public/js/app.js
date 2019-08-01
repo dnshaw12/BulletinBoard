3a978710c14f3bbbdfda782472674f3a7b94ab3a
Dan <dnshaw12@gmail.com>
2019-08-01 12:14:05 -0500

    profile pic can now be expanded to view larger

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,37 @@
-$('.page-content').on('click', (e) => {
+$('.profile-pic').on('click', (e) => {
 	console.log("WORKING");
 	console.log(e);
+
+	const $overlay = $(`<div id="overlay" />`)
+	$overlay.css({
+		'background-color': 'rgba(13,55,13,0.5)',
+		'width': '100vw',
+		'height': '100vh',
+		'position': 'fixed',
+		'z-index': '10',
+		'top': '0px',
+		'display': 'flex',
+		'flex-direction': 'row',
+		'justify-content': 'flex-start',
+		'align-items': 'center'
+	})
+
+	$overlay.on('click',() => {
+		$('#overlay').remove()
+	})
+
+	$img = $(`<img src="${e.currentTarget.src}"/>`)
+
+	$img.css({
+		'opacity': '1',
+		'margin': 'auto',
+		'top': '4.7em',
+		'border': '10px solid rgb(13,55,13)',
+		'max-height': '75%'
+
+	})
+
+	$overlay.append($img)
+
+	$('body').append($overlay)
 })
